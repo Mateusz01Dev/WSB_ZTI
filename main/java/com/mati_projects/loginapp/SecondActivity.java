9355a0c75a3b300cea3ee97a50a8cb6c231bb16f
Mateusz <33101240+Mateusz01Dev@users.noreply.github.com>
2020-06-25 20:28:27 +0200

    Add files via upload

---
+++ main/java/com/mati_projects/loginapp/SecondActivity.java
@@ -0,0 +1,14 @@
+package com.mati_projects.loginapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
